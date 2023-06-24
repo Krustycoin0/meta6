89ac446e74bd19b207375f09232d8551c25b3a04
Krustycoin0 <133280489+Krustycoin0@users.noreply.github.com>
2023-06-24 11:18:07 +0200

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
-export { default as Blog } from './blog/Blog';
-export { default as Features } from './features/Features';
-export { default as Footer } from './footer/Footer';
-export { default as Header } from './header/Header';
-export { default as Possibility } from './possibility/Possibility';
-export { default as WhatGPT3 } from './whatGPT3/WhatGPT3';
+export { default as Article } from './article/Article';
+export { default as Brand } from './brand/Brand';
+export { default as Cta } from './cta/Cta';
+export { default as Feature } from './feature/Feature';
+export { default as Navbar } from './navbar/Navbar';
